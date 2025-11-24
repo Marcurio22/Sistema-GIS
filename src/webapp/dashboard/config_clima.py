f38647c73752a8aae9ad921ee1625c9399ad3616
Hugo Sainz <hsainz@ubu.es>
2025-11-24 13:15:32 +0100

    nuevo archivo config_clima.py

---
+++ src/webapp/dashboard/config_clima.py
@@ -0,0 +1,167 @@
+estados_cielo = {
+                # Despejado
+                '11': 'Despejado', '11n': 'Despejado',
+                
+                # Nubosidad sin precipitación
+                '12': 'Poco nuboso', '12n': 'Poco nuboso',
+                '13': 'Intervalos nubosos', '13n': 'Intervalos nubosos',
+                '14': 'Nuboso', '14n': 'Nuboso',
+                '15': 'Muy nuboso', '15n': 'Muy nuboso',
+                '16': 'Cubierto', '16n': 'Cubierto',
+                '17': 'Nubes altas', '17n': 'Nubes altas',
+                
+                # Con lluvia escasa
+                '13d': 'Intervalos nubosos con lluvia escasa', '13dn': 'Intervalos nubosos con lluvia escasa',
+                '14d': 'Nuboso con lluvia escasa', '14dn': 'Nuboso con lluvia escasa',
+                '15d': 'Muy nuboso con lluvia escasa', '15dn': 'Muy nuboso con lluvia escasa',
+                '16d': 'Cubierto con lluvia escasa', '16dn': 'Cubierto con lluvia escasa',
+                
+                # Con lluvia
+                '23': 'Intervalos nubosos con lluvia', '23n': 'Intervalos nubosos con lluvia',
+                '24': 'Nuboso con lluvia', '24n': 'Nuboso con lluvia',
+                '25': 'Muy nuboso con lluvia', '25n': 'Muy nuboso con lluvia',
+                '26': 'Cubierto con lluvia', '26n': 'Cubierto con lluvia',
+                '27': 'Chubascos', '27n': 'Chubascos',
+                
+                # Con nieve escasa
+                '33d': 'Intervalos nubosos con nieve escasa', '33dn': 'Intervalos nubosos con nieve escasa',
+                '34d': 'Nuboso con nieve escasa', '34dn': 'Nuboso con nieve escasa',
+                '35d': 'Muy nuboso con nieve escasa', '35dn': 'Muy nuboso con nieve escasa',
+                '36d': 'Cubierto con nieve escasa', '36dn': 'Cubierto con nieve escasa',
+                
+                # Con nieve
+                '43': 'Intervalos nubosos con nieve', '43n': 'Intervalos nubosos con nieve',
+                '44': 'Nuboso con nieve', '44n': 'Nuboso con nieve',
+                '45': 'Muy nuboso con nieve', '45n': 'Muy nuboso con nieve',
+                '46': 'Cubierto con nieve', '46n': 'Cubierto con nieve',
+                '47': 'Chubascos de nieve', '47n': 'Chubascos de nieve',
+                
+                # Con tormenta
+                '51': 'Intervalos nubosos con tormenta', '51n': 'Intervalos nubosos con tormenta',
+                '52': 'Nuboso con tormenta', '52n': 'Nuboso con tormenta',
+                '53': 'Muy nuboso con tormenta', '53n': 'Muy nuboso con tormenta',
+                '54': 'Cubierto con tormenta', '54n': 'Cubierto con tormenta',
+                
+                # Con tormenta y lluvia escasa
+                '61': 'Intervalos nubosos con tormenta y lluvia escasa', '61n': 'Intervalos nubosos con tormenta y lluvia escasa',
+                '62': 'Nuboso con tormenta y lluvia escasa', '62n': 'Nuboso con tormenta y lluvia escasa',
+                '63': 'Muy nuboso con tormenta y lluvia escasa', '63n': 'Muy nuboso con tormenta y lluvia escasa',
+                '64': 'Cubierto con tormenta y lluvia escasa', '64n': 'Cubierto con tormenta y lluvia escasa',
+                
+                # Niebla, bruma y calima
+                '81': 'Niebla', '81n': 'Niebla',
+                '82': 'Bruma', '82n': 'Bruma',
+                '83': 'Calima', '83n': 'Calima',
+            }
+
+iconos_bootstrap = {
+                # Despejado
+                '11': 'sun-fill', '11n': 'moon-stars-fill',
+                
+                # Nubosidad sin precipitación
+                '12': 'cloud-sun-fill', '12n': 'cloud-moon-fill',
+                '13': 'clouds-fill', '13n': 'clouds-fill',
+                '14': 'cloud-fill', '14n': 'cloud-fill',
+                '15': 'cloud-fill', '15n': 'cloud-fill',
+                '16': 'cloud-fill', '16n': 'cloud-fill',
+                '17': 'clouds', '17n': 'clouds',
+                
+                # Con lluvia escasa
+                '13d': 'cloud-drizzle-fill', '13dn': 'cloud-drizzle-fill',
+                '14d': 'cloud-drizzle-fill', '14dn': 'cloud-drizzle-fill',
+                '15d': 'cloud-drizzle-fill', '15dn': 'cloud-drizzle-fill',
+                '16d': 'cloud-drizzle-fill', '16dn': 'cloud-drizzle-fill',
+                
+                # Con lluvia
+                '23': 'cloud-rain-fill', '23n': 'cloud-rain-fill',
+                '24': 'cloud-rain-heavy-fill', '24n': 'cloud-rain-heavy-fill',
+                '25': 'cloud-rain-heavy-fill', '25n': 'cloud-rain-heavy-fill',
+                '26': 'cloud-rain-heavy-fill', '26n': 'cloud-rain-heavy-fill',
+                '27': 'cloud-rain-heavy-fill', '27n': 'cloud-rain-heavy-fill',
+                
+                # Con nieve escasa
+                '33d': 'cloud-snow', '33dn': 'cloud-snow',
+                '34d': 'cloud-snow', '34dn': 'cloud-snow',
+                '35d': 'cloud-snow', '35dn': 'cloud-snow',
+                '36d': 'cloud-snow', '36dn': 'cloud-snow',
+                
+                # Con nieve
+                '43': 'cloud-snow-fill', '43n': 'cloud-snow-fill',
+                '44': 'cloud-snow-fill', '44n': 'cloud-snow-fill',
+                '45': 'cloud-snow-fill', '45n': 'cloud-snow-fill',
+                '46': 'cloud-snow-fill', '46n': 'cloud-snow-fill',
+                '47': 'cloud-snow-fill', '47n': 'cloud-snow-fill',
+                
+                # Con tormenta
+                '51': 'cloud-lightning-rain-fill', '51n': 'cloud-lightning-rain-fill',
+                '52': 'cloud-lightning-rain-fill', '52n': 'cloud-lightning-rain-fill',
+                '53': 'cloud-lightning-rain-fill', '53n': 'cloud-lightning-rain-fill',
+                '54': 'cloud-lightning-rain-fill', '54n': 'cloud-lightning-rain-fill',
+                
+                # Con tormenta y lluvia escasa
+                '61': 'cloud-lightning-fill', '61n': 'cloud-lightning-fill',
+                '62': 'cloud-lightning-fill', '62n': 'cloud-lightning-fill',
+                '63': 'cloud-lightning-fill', '63n': 'cloud-lightning-fill',
+                '64': 'cloud-lightning-fill', '64n': 'cloud-lightning-fill',
+                
+                # Niebla, bruma y calima
+                '81': 'cloud-fog-fill', '81n': 'cloud-fog-fill',
+                '82': 'cloud-haze-fill', '82n': 'cloud-haze-fill',
+                '83': 'cloud-haze-fill', '83n': 'cloud-haze-fill',
+            }
+colores_iconos = {
+                    '11': 'text-warning', '11n': 'text-white',
+                    
+                    # Poco nuboso - amarillo suave
+                    '12': 'text-warning', '12n': 'text-white',
+                    
+                    # Nuboso - gris
+                    '13': 'text-secondary', '13n': 'text-secondary',
+                    '14': 'text-secondary', '14n': 'text-secondary',
+                    '15': 'text-secondary', '15n': 'text-secondary',
+                    '16': 'text-secondary', '16n': 'text-secondary',
+                    '17': 'text-secondary', '17n': 'text-secondary',
+                    
+                    # Lluvia escasa - azul claro
+                    '13d': 'text-info', '13dn': 'text-info',
+                    '14d': 'text-info', '14dn': 'text-info',
+                    '15d': 'text-info', '15dn': 'text-info',
+                    '16d': 'text-info', '16dn': 'text-info',
+                    
+                    # Lluvia - azul oscuro
+                    '23': 'text-primary', '23n': 'text-primary',
+                    '24': 'text-primary', '24n': 'text-primary',
+                    '25': 'text-primary', '25n': 'text-primary',
+                    '26': 'text-primary', '26n': 'text-primary',
+                    '27': 'text-primary', '27n': 'text-primary',
+                    
+                    # Nieve escasa - celeste
+                    '33d': 'text-info', '33dn': 'text-info',
+                    '34d': 'text-info', '34dn': 'text-info',
+                    '35d': 'text-info', '35dn': 'text-info',
+                    '36d': 'text-info', '36dn': 'text-info',
+                    
+                    # Nieve - blanco/celeste
+                    '43': 'text-light', '43n': 'text-light',
+                    '44': 'text-light', '44n': 'text-light',
+                    '45': 'text-light', '45n': 'text-light',
+                    '46': 'text-light', '46n': 'text-light',
+                    '47': 'text-light', '47n': 'text-light',
+                    
+                    # Tormenta - morado/oscuro
+                    '51': 'text-danger', '51n': 'text-danger',
+                    '52': 'text-danger', '52n': 'text-danger',
+                    '53': 'text-danger', '53n': 'text-danger',
+                    '54': 'text-danger', '54n': 'text-danger',
+                    
+                    # Tormenta con lluvia escasa - naranja
+                    '61': 'text-warning', '61n': 'text-warning',
+                    '62': 'text-warning', '62n': 'text-warning',
+                    '63': 'text-warning', '63n': 'text-warning',
+                    '64': 'text-warning', '64n': 'text-warning',
+                    
+                    # Niebla/bruma/calima - gris claro
+                    '81': 'text-secondary', '81n': 'text-secondary',
+                    '82': 'text-secondary', '82n': 'text-secondary',
+                    '83': 'text-secondary', '83n': 'text-secondary',
+                }
