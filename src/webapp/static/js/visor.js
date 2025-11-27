f55a282032df8d66c7562a939a6d576ce52cbfc3
Marcurio22 <mzl1004@alu.ubu.es>
2025-11-27 15:10:04 +0100

    Funcionalidad de visor de capas añadida

--- src/webapp/static/js/visor.js
+++ src/webapp/static/js/visor.js
@@ -8,9 +8,9 @@ L.tileLayer("https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
 
 // 2) WMS SIGPAC (cascaded desde tu GeoServer)
 const sigpacRecintos = L.tileLayer.wms(
-  "http://localhost:8080/geoserver/gis_project/wms",
+  "http://100.102.237.86:8080/geoserver/gis_project/wms",
   {
-    layers: "AU.Sigpac:recinto",   // ⚠️ confirma nombre exacto en tu store
+    layers: "gis_project:recintos",
     format: "image/png",
     transparent: true,
     tiled: true
