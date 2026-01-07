2a70b2be82c3d1078e0970498b08263014bc46cf
Hugo Sainz <hsainz@ubu.es>
2026-01-07 09:13:17 +0100

    Quitar imports sin uso

--- src/webapp/__init__.py
+++ src/webapp/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, flash, redirect, url_for
+from flask import Flask, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_mail import Mail
