098ca1f838c555783cabd37ba67841b905069a19
Hugo Sainz <hsainz@ubu.es>
2026-07-09 12:05:29 +0200

    Arreglar bat 2

--- ejecutar-ndvi-completo.bat
+++ ejecutar-ndvi-completo.bat
@@ -4,13 +4,11 @@ REM Uso desde la carpeta de la comunidad:
 REM   ejecutar-ndvi-completo.bat
 REM Tambien acepta ruta explicita:
 REM   ejecutar-ndvi-completo.bat C:\GIS\comunidades\mi_comunidad
-setlocal EnableExtensions
+setlocal EnableExtensions EnableDelayedExpansion
 
 set "DIR=%~1"
-if "%DIR%"=="" (
-  set "DIR=%~dp0"
-  if "%DIR:~-1%"=="\" set "DIR=%DIR:~0,-1%"
-)
+if "%DIR%"=="" set "DIR=%~dp0"
+if "!DIR:~-1!"=="\" set "DIR=!DIR:~0,-1!"
 
 if not exist "%DIR%\src\ndvi_completo.py" (
   echo.
@@ -19,10 +17,14 @@ if not exist "%DIR%\src\ndvi_completo.py" (
   goto :fin_error
 )
 
-set "PY=C:\Users\UBU\anaconda3\envs\gis\python.exe"
+REM --- Entorno conda gis (mismo que al hacer "conda activate gis") ---
+set "ENV_ROOT=C:\Users\UBU\anaconda3\envs\gis"
+set "PY=%ENV_ROOT%\python.exe"
 if not exist "%PY%" set "PY=python"
 
-set "ENV_ROOT=C:\Users\UBU\anaconda3\envs\gis"
+if exist "%ENV_ROOT%\python.exe" (
+  set "PATH=%ENV_ROOT%;%ENV_ROOT%\Library\mingw-w64\bin;%ENV_ROOT%\Library\usr\bin;%ENV_ROOT%\Library\bin;%ENV_ROOT%\Scripts;%ENV_ROOT%\bin;%PATH%"
+)
 if exist "%ENV_ROOT%\Library\share\proj" set "PROJ_LIB=%ENV_ROOT%\Library\share\proj"
 if exist "%ENV_ROOT%\Library\share\gdal" set "GDAL_DATA=%ENV_ROOT%\Library\share\gdal"
 set "PYTHONUNBUFFERED=1"
@@ -48,7 +50,8 @@ echo [OK] Terminado.
 goto :fin_ok
 
 :fin_error
-echo [ERROR] NDVI completo fallo.
+echo.
+echo [ERROR] NDVI completo fallo. Revisa el mensaje de arriba.
 if "%NO_PAUSE%"=="1" exit /b 1
 pause
 exit /b 1
